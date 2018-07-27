9788da3f23bc8d11563481a01b37b4d9db92b8e1
PatODay <poday22@gmail.com>
2018-07-27 10:24:51 -0400

    Pushing to Heroku
    Installed linter and pushing to Heroku

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 'use strict'
 // Requiring twitter package
 const TwitterPackage = require('twitter');
+// Creating a JS object containing keys/tokens associated with this account
+const keys = {
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token_key: process.env.ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
+
+const Twitter = new TwitterPackage(keys);
+
+Twitter.post('statuses/update', {status: 'Testing'},  function(error, tweet, response){
+  if(error){
+    console.log(error);
+  }
+  console.log(tweet);  // Tweet body.
+  console.log(response);  // Raw response object.
+});
